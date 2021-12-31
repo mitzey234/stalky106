fa78e3abf54696a44ccd237f842cf032003dd7ce
Alexander Young <alex1001@live.ca>
2021-12-30 21:39:35 -0700

    Update to v4

--- Stalky106/Plugin.cs
+++ Stalky106/Plugin.cs
@@ -19,28 +19,29 @@ namespace Stalky106
 		public void NewCoroutine(IEnumerator<float> coroutine, MEC.Segment segment = MEC.Segment.Update) => coroutines.Add(MEC.Timing.RunCoroutine(coroutine, segment));
 		public override string Prefix => "ST106";
 		public static readonly Vector3 pocketDimension = new Vector3(0f, -1998f, 0f);
+		private bool state = false;
 
 		private EventHandlers events;
 		public StalkyMethods Methods { private set; get; }
 
 		public override void OnEnabled()
 		{
-			if (!Config.IsEnabled)
-			{
-				Log.Info("Stalky106 is disabled via configs. It will not be loaded.");
-				return;
-			}
-			Log.Info("Prepare to face Larry...");
+			if (state) return;
+
 			Methods = new StalkyMethods(this);
 			events = new EventHandlers(this);
 			SvEvents.RoundStarted += events.OnRoundStart;
 			PlyEvents.ChangingRole += events.OnSetClass;
 			ScpEvents.CreatingPortal += events.OnCreatePortal;
+
+			state = true;
 			base.OnEnabled();
 		}
 
 		public override void OnDisabled()
 		{
+			if (!state) return;
+
 			if (coroutines != null && coroutines.Count > 0) MEC.Timing.KillCoroutines(coroutines.ToArray());
 			if (events != null)
 			{
@@ -52,11 +53,8 @@ namespace Stalky106
 			
 			Methods = null;
 
-			if (Config.IsEnabled)
-			{
-				Log.Info("Larry won't ever stalk you again at night...");
-				base.OnDisabled();
-			}
+			state = false;
+			base.OnDisabled();
 		}
 	}
 }
